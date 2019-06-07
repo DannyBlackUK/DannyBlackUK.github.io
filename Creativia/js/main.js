6caec373163a5fa465912a1b0c104df4a969b316
onlyblackdanny@gmail.com <onlyblackdanny@gmail.com>
2019-06-08 02:44:58 +0300

    222

--- Creativia/js/main.js
+++ Creativia/js/main.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
 	
+	//burger-menu
 	$('.burger-icon').click(function() {
 		$('.burger-menu__wrapper').slideToggle();
 		$(this).toggleClass('active-burger-menu');
@@ -12,9 +13,55 @@ jQuery(document).ready(function($) {
 		$('.burger-icon').toggleClass('active-burger-menu');
 	});
 
+	//
 
 
 
+//tab in second-screen
 
+	$('.tab-content').hide();
+	$('.tab-menu > a').click(function() {
+  	
+
+    var findArticle = $(this).next();
+    var findWrapper = $(this).closest('.tab-menu');
+      
+	// $('.tab-menu > a').animate({color: "red"}, 1000);
+	$(this).addClass('tab-button__active').removeClass('tab-button__active', 2000)
+
+    if (findArticle.is(':visible')) {
+      findArticle.slideUp('fast');
+
+    }
+    else {
+      findWrapper.find('>article').slideUp('fast');
+      findArticle.slideDown('fast');
+    }
+  });
 
 });
+
+
+
+
+
+
+
+//
+
+	//slider in second-screen
+
+//	
+
+
+
+
+
+
+
+
+
+
+
+
+
